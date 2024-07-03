84ba3dff2ee4e404bbe2c411144d2d381a888dcb
peace220 <jingzhisim0123@gmail.com>
2024-07-03 23:58:33 +0800

    added questions

--- frontend/src/Routes/Routes.jsx
+++ frontend/src/Routes/Routes.jsx
@@ -3,6 +3,21 @@ import Signup from "../pages/Signup/Signup";
 import Home from "../pages/Home";
 import Collaboration from "../pages/Collaboration";
 import Discover from "../pages/Discover"
+import QuestionForm from "../components/instructor/questions/questionsforms";
+import UserQuestion from "../components/instructor/questions/userQuestions";
+const sampleQuestions = [
+  {
+    id: 1,
+    text: 'What is the capital of France?',
+    type: 'multiple-choice',
+    choices: ['Paris', 'London', 'Berlin', 'Madrid'],
+  },
+  {
+    id: 2,
+    text: 'Explain the theory of relativity.',
+    type: 'essay',
+  },
+];
 const LoggedinRoutes = [
   {
     path: "/",
@@ -24,5 +39,13 @@ const LoggedinRoutes = [
     path: "/discover",
     element: <Discover />,
   },
+  {
+    path:"/questionform",
+    element: <QuestionForm/>,
+  },
+  {
+    path:"/userquestions",
+    element: <UserQuestion questions={sampleQuestions}/>,
+  }
 ];
 export default LoggedinRoutes;
