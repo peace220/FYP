1a5b2294ed67d62a06dab5cbf6296aa45dcbf632
peace220 <jingzhisim0123@gmail.com>
2024-07-27 02:15:56 +0800

    change theme

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,58 +1,81 @@
 import React, { useState } from "react";
 import Layout from "../Layout/Layout1";
 import axios from "axios";
+import { useThemedStyles } from "../../hooks/ThemeContrast";
 import { useNavigate } from "react-router-dom";
+
 const Login = () => {
+  const { backgroundColor, textColor, cardBackground } = useThemedStyles();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState(null);
   const navigate = useNavigate();
+
   const handleLogin = async () => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      setEmailError("Please enter a valid email address.");
+      return;
+    } else {
+      setEmailError(null);
+    }
+
     try {
       const response = await axios.post(
         "http://localhost:5000/api/auth/login",
-        { email, password }
+        {
+          email,
+          password,
+        }
       );
-      alert("Login successful");
+
       localStorage.setItem("token", response.data.token);
-      navigate(`/`);
+      navigate("/");
     } catch (error) {
-      alert("Invalid credentials");
+      alert("Invalid email or password");
     }
   };
 
   return (
     <Layout>
-      <div className="flex justify-center items-center h-full bg-gray-100">
-        <div className="w-full max-w-md">
-          <form
-            className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
-            onSubmit={(e) => e.preventDefault()}
-          >
-            <h2 className="text-center text-2xl mb-4">Login</h2>
+      <div
+        className={`flex justify-center items-center h-full ${backgroundColor}`}
+      >
+        <div
+          className={`w-full max-w-md ${cardBackground} shadow-md rounded px-8 pt-6 pb-8 mb-4`}
+        >
+          <h2 className={`text-center text-2xl mb-4 ${textColor}`}>Log In</h2>
+          <form className="" onSubmit={(e) => e.preventDefault()}>
             <div className="mb-4">
               <label
-                className="block text-gray-700 text-sm font-bold mb-2"
+                className={`block ${textColor} text-sm font-bold mb-2`}
+                htmlFor="email"
               >
                 Email
               </label>
               <input
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                className={`shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline`}
                 id="email"
                 type="email"
                 placeholder="Email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
               />
+              {emailError && (
+                <p className={`text-red-500 text-xs italic ${textColor}`}>
+                  {emailError}
+                </p>
+              )}
             </div>
             <div className="mb-6">
               <label
-                className="block text-gray-700 text-sm font-bold mb-2"
+                className={`block ${textColor} text-sm font-bold mb-2`}
                 htmlFor="password"
               >
                 Password
               </label>
               <input
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                className={`shadow appearance-none border rounded w-full py-2 px-3  mb-3 leading-tight focus:outline-none focus:shadow-outline`}
                 id="password"
                 type="password"
                 placeholder="***********"
@@ -62,11 +85,11 @@ const Login = () => {
             </div>
             <div className="flex items-center justify-between">
               <button
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline`}
                 type="button"
                 onClick={handleLogin}
               >
-                Sign In
+                Log In
               </button>
             </div>
           </form>
--- frontend/src/pages/Signup/Signup.jsx
+++ frontend/src/pages/Signup/Signup.jsx
@@ -1,20 +1,33 @@
 import React, { useState } from "react";
 import Layout from "../Layout/Layout1";
 import axios from "axios";
-
+import { useThemedStyles } from "../../hooks/ThemeContrast";
+import { useNavigate } from "react-router-dom";
 const Signup = () => {
+  const navigate = useNavigate();
+  const { backgroundColor, textColor, cardBackground } = useThemedStyles();
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState(null);
 
   const handleSignup = async () => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      setEmailError("Please enter a valid email address.");
+      return;
+    } else {
+      setEmailError(null);
+    }
+
     try {
       await axios.post("http://localhost:5000/api/auth/signup", {
         username,
         email,
         password,
       });
-      alert("Signup successful");
+      alert("Signup successful")
+      navigate("/join/login");
     } catch (error) {
       alert("Error during signup");
     }
@@ -22,22 +35,22 @@ const Signup = () => {
 
   return (
     <Layout>
-      <div className="flex justify-center items-center h-full bg-gray-100">
-        <div className="w-full max-w-md">
+      <div className={`flex justify-center items-center h-full ${backgroundColor}`}>
+        <div className={`w-full max-w-md ${cardBackground} shadow-md rounded px-8 pt-6 pb-8 mb-4`}>
+          <h2 className={`text-center text-2xl mb-4 ${textColor}`}>Sign Up</h2>
           <form
-            className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+            className=""
             onSubmit={(e) => e.preventDefault()}
           >
-            <h2 className="text-center text-2xl mb-4">Sign Up</h2>
             <div className="mb-4">
               <label
-                className="block text-gray-700 text-sm font-bold mb-2"
+                className={`block ${textColor} text-sm font-bold mb-2`}
                 htmlFor="username"
               >
                 Username
               </label>
               <input
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                className={`shadow appearance-none border rounded w-full py-2 px-3  leading-tight focus:outline-none focus:shadow-outline`}
                 id="username"
                 type="text"
                 placeholder="Username"
@@ -47,29 +60,32 @@ const Signup = () => {
             </div>
             <div className="mb-4">
               <label
-                className="block text-gray-700 text-sm font-bold mb-2"
+                className={`block ${textColor} text-sm font-bold mb-2`}
                 htmlFor="email"
               >
                 Email
               </label>
               <input
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                className={`shadow appearance-none border rounded w-full py-2 px-3  leading-tight focus:outline-none focus:shadow-outline`}
                 id="email"
                 type="email"
                 placeholder="Email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
               />
+              {emailError && (
+                <p className={`text-red-500 text-xs italic ${textColor}`}>{emailError}</p>
+              )}
             </div>
             <div className="mb-6">
               <label
-                className="block text-gray-700 text-sm font-bold mb-2"
+                className={`block ${textColor} text-sm font-bold mb-2`}
                 htmlFor="password"
               >
                 Password
               </label>
               <input
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                className={`shadow appearance-none border rounded w-full py-2 px-3 mb-3 leading-tight focus:outline-none focus:shadow-outline`}
                 id="password"
                 type="password"
                 placeholder="***********"
@@ -79,7 +95,7 @@ const Signup = () => {
             </div>
             <div className="flex items-center justify-between">
               <button
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline`}
                 type="button"
                 onClick={handleSignup}
               >
